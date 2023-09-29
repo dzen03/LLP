ad080af6dd4a2e264b1d3875d0696c99c5fa96b0
dzen03 <43926347+dzen03@users.noreply.github.com>
2023-09-29 12:12:28 +0300

    init

---
+++ Lab1/solution/include/metadata.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef LAB1_SOLUTION_SRC_METADATA_H_
+#define LAB1_SOLUTION_SRC_METADATA_H_
+
+struct metadata
+{
+  int a;
+};
+
+#endif //LAB1_SOLUTION_SRC_METADATA_H_
---
+++ Lab1/solution/src/main.c
@@ -0,0 +1,12 @@
+#define _FILE_OFFSET_BITS 64
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+
+  return 0;
+}
