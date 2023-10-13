63bc3f46934b797aac3ce9bfcc43d236f119834f
dzen03 <43926347+dzen03@users.noreply.github.com>
2023-10-13 04:24:21 +0300

    fixed file_read_write test

--- Lab1/tester/tests/file_read_write.c
+++ Lab1/tester/tests/file_read_write.c
@@ -1,7 +1,5 @@
 #include "backend.h"
 #include "file.h"
-#include "graph.h"
-#include "relationship.h"
 
 #include <assert.h>
 
@@ -15,16 +13,7 @@ int main(void)
   struct file file = get_file();
 
   assert(file.metadata.first_node_addr != 0);
-  assert(file.metadata.first_relationship_addr != 0);
   assert(file.metadata.last_node_addr != 0);
-  assert(file.metadata.last_relationship_addr != 0);
-
-  struct relationship relationship;
-  relationship_read(&relationship, file.metadata.first_relationship_addr);
-
-  print_node(relationship.first_node_addr);
-  print_node(relationship.second_node_addr);
-
 
   backend_stop();
   return 0;
